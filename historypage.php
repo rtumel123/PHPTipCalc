09c5bd7ee504d23070c301e751068decef0f5c35
rtumel123 <59629112+rtumel123@users.noreply.github.com>
2020-03-12 20:24:32 -0400

    bootstrapped history page

--- historypage.php
+++ historypage.php
@@ -6,10 +6,10 @@ $dbname = "tips";
 
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
- 
+
     $sql = 'SELECT * FROM tiplog
               ORDER BY tipAmt';
- 
+
     $q = $pdo->query($sql);
     $q->setFetchMode(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
@@ -18,32 +18,48 @@ try {
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>PHP MySQL Query Data Demo</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-        <div id="container">
-            <h1>Customer History</h1>
-            <table class="table table-bordered table-condensed">
-                <thead>
+
+<head>
+    <title>Customer History</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div id="container">
+        <h1>Customer History</h1>
+        <table class="table table-bordered table-condensed">
+            <thead>
+                <tr>
+                    <th>Party Name</th>
+                    <th>Bill Amount</th>
+                    <th>Service Quality</th>
+                    <th>Number of People</th>
+                    <th>Tip Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($row = $q->fetch()) : ?>
                     <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Job Title</th>
+                        <td><?php echo htmlspecialchars($row['partyName']) ?></td>
+                        <td><?php echo htmlspecialchars($row['billAmt']); ?></td>
+                        <td><?php echo htmlspecialchars($row['serviceLevel']); ?></td>
+                        <td><?php echo htmlspecialchars($row['numPeople']); ?></td>
+                        <td><?php echo htmlspecialchars($row['tipAmt']); ?></td>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php while ($row = $q->fetch()): ?>
-                        <tr>
-                            <td><?php echo htmlspecialchars($row['partyName']) ?></td>
-                            <td><?php echo htmlspecialchars($row['billAmt']); ?></td>
-                            <td><?php echo htmlspecialchars($row['serviceLevel']); ?></td>
-                        </tr>
-                    <?php endwhile; ?>
-                </tbody>
-            </table>
-    </body>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+</body>
 </div>
+
 </html>
